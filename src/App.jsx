5e6b27416bf55cd4bc06f7e1d56f398e710af0bf
Timur Ermolaev <timkogas@gmail.com>
2023-02-21 17:01:34 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Layout from './components/Layout/Layout';
 import MainPage from './containers/MainPage/MainPage';
 
 function App() {
-  
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout/>}>
-          <Route path='/' element={<MainPage/>}/>
+        <Route element={<Layout />}>
+          <Route path='/' element={<MainPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
